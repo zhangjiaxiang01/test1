3c5eae2dedc371eb358056181ee2223976328769
name <email@example.com:1160322782@qq.com>
2018-04-12 18:53:48 +0800

    第三次提交

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -1,9 +1,14 @@
+
+
 /**
  * Created by admin on 2018/4/12.
  */
 public class test1 {
-    public static void main (String[] args){
-        System.out.print("hellogit");
-        System.out.println("hellogit2");
-    }
+
+      public static void main(String args[]){
+          System.out.print("hellogit");
+          System.out.println("hellogit2");
+          System.out.println("hellogit3");//修改
+      }
+
 }
